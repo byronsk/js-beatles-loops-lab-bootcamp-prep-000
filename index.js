c750d6f3321c58e94d654dcb06c4fdd48ce125e6
vyskoutaris <vyskoutaris@hotmail.com>
2019-01-14 19:47:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ var musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo Star
 var instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
 
 
-theBeatlesPlay(musicians, instruments); 
-
 function theBeatlesPlay(musicians, instruments) {
-  var newArr = []; 
+  var array = []; 
   for (var i =0; i < musicians.length; i++) {
-    newArr.push(musicians[i] + " plays " + instruments[i]);
+    array.push(musicians[i] + " plays " + instruments[i]);
   }
-  return newArr;
+  return array;
 }
 
 
